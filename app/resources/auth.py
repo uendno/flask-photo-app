57ecda87233f363261ea692bb9319ae57ee89929
duongle26 <leduong26298@gmail.com>
2020-08-03 16:49:34 +0700

    refactor: rename variable

--- app/resources/auth.py
+++ app/resources/auth.py
@@ -13,13 +13,13 @@ auth_blueprint = Blueprint('auth_blueprint', __name__)
 @auth_blueprint.route('', methods=['POST'])
 def authenticate_user():
     try:
-        user_data = AuthSchema().load(request.get_json())
+        data = AuthSchema().load(request.get_json())
     except ValidationError as err:
         return jsonify(message="Invalid email or password"), 400
 
-    user = UserModel.query.filter_by(email=user_data['email']).one_or_none()
+    user = UserModel.query.filter_by(email=data['email']).one_or_none()
 
-    if check_password_hash(user.password, user_data['password']):
+    if check_password_hash(user.password, data['password']):
         payload = UserSchema(exclude=('password',)).dump(user)
         encoded_jwt = encode_token(payload)
         return jsonify(access_token=encoded_jwt.decode('UTF-8')), 200
--- app/resources/user.py
+++ app/resources/user.py
@@ -12,11 +12,11 @@ user_blueprint = Blueprint('user_blueprint', __name__)
 @user_blueprint.route('', methods=['POST'])
 def create_user():
     try:
-        user_data = UserSchema().load(request.get_json())
+        data = UserSchema().load(request.get_json())
     except ValidationError as err:
         return jsonify(message=err.messages), 400
 
-    new_user = UserModel(user_data['name'], user_data['email'], user_data['password'])
+    new_user = UserModel(data['name'], data['email'], data['password'])
     db.session.add(new_user)
     db.session.commit()
     return jsonify({}), 201
