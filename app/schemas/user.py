7bcb6f5004fbaf4212466871b6d79021a6abc25b
duongle26 <leduong26298@gmail.com>
2020-08-05 01:45:47 +0700

    fix: email and password are excluded from request

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -6,13 +6,10 @@ from app.models.user import UserModel
 class UserSchema(Schema):
     id = fields.Integer()
     name = fields.String(validate=validate.Length(min=1, max=30), required=True)
-    email = fields.Email(validate=validate.Length(max=30), required=True)
-    password = fields.String(validate=validate.Length(min=6), required=True)
+    email = fields.Email(validate=validate.Length(max=30), required=True, load_only=True)
+    password = fields.String(validate=validate.Length(min=6), required=True, load_only=True)
 
     @validates('email')
     def validate_email(self, value):
         if UserModel.query.filter_by(email=value).one_or_none():
             raise ValidationError('Email already exists.')
-
-    class Meta:
-        exclude = ("email", "password")
