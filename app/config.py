bf4ddadebef51c38aa591f0fc3f1b02ebbd44928
duongle26 <leduong26298@gmail.com>
2020-08-04 14:42:18 +0700

    fix: validate environment variable

--- app/config.py
+++ app/config.py
@@ -1,7 +1,12 @@
 import os
 from importlib import import_module
 
+list_env = ('dev', 'test', 'prod')
+
 env = os.getenv('ENV')
-config_name = 'app.cfg.' + env
-module = import_module(config_name)
-config = module.Config
+if env in list_env:
+    config_name = 'app.cfg.' + env
+    module = import_module(config_name)
+    config = module.Config
+else:
+    raise RuntimeError("Value for environment variable ENV is not valid")
