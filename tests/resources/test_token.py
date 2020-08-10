1d2c09c03b1c086f7d393d05460646e19f10489c
duongle26 <leduong26298@gmail.com>
2020-08-10 10:56:18 +0700

    refactor: remove pytest-mock and use standard library as replacement

--- tests/resources/test_token.py
+++ tests/resources/test_token.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import flask
 
 from app import create_app
@@ -5,29 +7,27 @@ from app.utils import token
 from tests.utils.token import get_access_token
 
 
-def test_missing_access_token(mocker):
-    response = mock_header_and_test_token(mocker, None)
+def test_missing_access_token():
+    response = mock_header_and_test_token(None)
     assert response == 401
 
 
-def test_invalid_token(mocker):
-    response = mock_header_and_test_token(mocker, f'Bearer iikshf92.oifhsfds.98dfdsfh')
+def test_invalid_token():
+    response = mock_header_and_test_token(f'Bearer iikshf92.oifhsfds.98dfdsfh')
     assert response == 401
 
 
-def test_valid_token(mocker, client):
+def test_valid_token(client):
     access_token = get_access_token(client)
-    response = mock_header_and_test_token(mocker, f'Bearer {access_token}')
+    response = mock_header_and_test_token(f'Bearer {access_token}')
     assert response == 200
 
 
-def mock_header_and_test_token(mocker, access_token):
-    app = create_app()
-    request_mock = mocker.patch.object(flask, 'request')
-    request_mock.headers.get.return_value = access_token
-
-    with app.app_context():
-        response = token.token_required(lambda value: (value, 200))()
+def mock_header_and_test_token(access_token):
+    with patch.object(flask, 'request') as request_mock:
+        request_mock.headers.get.return_value = access_token
+        with create_app().app_context():
+            response = token.token_required(lambda value: (value, 200))()
 
     request_mock.headers.get.assert_called_once_with('Authorization', None)
     return response[1]
