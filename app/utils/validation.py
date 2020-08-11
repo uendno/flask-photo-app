fc06f286bf5fe4dc0c8e87083e6dcf6bea8ad5f3
duongle26 <leduong26298@gmail.com>
2020-08-11 10:04:46 +0700

    feat: decorator to validate category, item, ownership

--- app/utils/validation.py
+++ app/utils/validation.py
@@ -3,6 +3,9 @@ from functools import wraps
 from flask import request, jsonify
 from marshmallow import ValidationError
 
+from app.models.category import CategoryModel
+from app.models.item import ItemModel
+
 
 def validate_schema(schema):
     def decorator(f):
@@ -13,8 +16,40 @@ def validate_schema(schema):
                 data = schema().load(request_data)
             except ValidationError as err:
                 return jsonify(message='Bad Request', error=err.messages), 400
-            return f(data, *args, **kwargs)
+            return f(data=data, *args, **kwargs)
 
         return wrapper
 
     return decorator
+
+
+def validate_category(f):
+    @wraps(f)
+    def wrapper(category_id, *args, **kwargs):
+        category = CategoryModel.query.get(category_id)
+        if not category:
+            return jsonify(message='Category not found'), 404
+        return f(category=category, category_id=category_id, *args, **kwargs)
+
+    return wrapper
+
+
+def validate_item(f):
+    @wraps(f)
+    def wrapper(item_id, *args, **kwargs):
+        item = ItemModel.query.get(item_id)
+        if not item:
+            return jsonify(message='Item not found'), 404
+        return f(item=item, item_id=item_id, *args, **kwargs)
+
+    return wrapper
+
+
+def validate_ownership(f):
+    @wraps(f)
+    def wrapper(item, user, *args, **kwargs):
+        if item.user_id != user.id:
+            return jsonify(message='The user is not authorized to perform this action'), 403
+        return f(item=item, user=user, *args, **kwargs)
+
+    return wrapper
