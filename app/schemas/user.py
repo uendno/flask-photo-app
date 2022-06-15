f33965213b6cd1bdd7c002bebf2e6384ae5e9aad
Thang Tran <uendno@gmail.com>
2022-06-15 20:48:33 +0700

    fix: Return user email

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -3,7 +3,7 @@ from marshmallow import Schema, fields, validate
 
 class CreateUserSchema(Schema):
     name = fields.String(validate=validate.Length(min=1, max=30), required=True)
-    email = fields.Email(validate=validate.Length(max=30), required=True, load_only=True)
+    email = fields.Email(validate=validate.Length(max=30), required=True)
     password = fields.String(validate=validate.Length(min=6), required=True, load_only=True)
 
 
